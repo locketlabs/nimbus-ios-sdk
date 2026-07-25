1ded659546c32c162b4139914de08d4e1c00f260
Qasim Iqbal <qasim196@gmail.com>
2026-07-24 22:30:58 -0400

    Remove unused mediation adapters

--- Package.swift
+++ Package.swift
@@ -23,59 +23,19 @@ let package = Package(
            name: "NimbusRequestKit",
            targets: ["NimbusRequestTarget"]),
         .library(
-           name: "NimbusGAMKit",
-           targets: ["NimbusGAMKit"]),
-        .library(
            name: "NimbusAdMobKit",
            targets: ["NimbusAdMobKit"]),
-        .library(
-           name: "NimbusGoogleKit",
-           targets: ["NimbusGoogleKit"]),
-        .library(
-           name: "NimbusFANKit",
-           targets: ["NimbusRenderFANKit", "NimbusRequestFANKit"]),
-        .library(
-           name: "NimbusLiveRampKit",
-           targets: ["NimbusLiveRampKit"]),
-        .library(
-           name: "NimbusRequestAPSKit",
-           targets: ["NimbusRequestAPSKit", "DTBiOSSDK"]),
-        .library(
-           name: "NimbusUnityKit",
-           targets: ["NimbusUnityKit", "UnityAds"]),
         .library(
            name: "NimbusVungleKit",
            targets: ["NimbusVungleKit"]),
         .library(
-           name: "NimbusMintegralKit",
-           targets: ["NimbusMintegralKit"]),
-        .library(
-           name: "NimbusMobileFuseKit",
-           targets: ["NimbusMobileFuseKit"]),
-        .library(
            name: "NimbusMolocoKit",
            targets: ["NimbusMolocoKit"]),
-        .library(
-           name: "NimbusInMobiKit",
-           targets: ["NimbusInMobiKit"]),
-        .library(
-           name: "NimbusRequestAPSKit-WithoutDTBiOSSDK",
-           targets: ["NimbusRequestAPSKit"]),
-        .library(
-           name: "NimbusFANKit-WithoutFBAudienceNetwork",
-           targets: ["NimbusRenderFANKit", "NimbusRequestFANKit"]),
-        .library(
-           name: "NimbusUnityKit-WithoutUnityAds",
-           targets: ["NimbusUnityKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/LiveRamp/ats-sdk-ios.git", from: "2.5.0"),
         .package(url: "https://github.com/birdrides/mockingbird.git", from: "0.20.0"),
         .package(url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager.git", from: "7.6.0"),
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", "12.0.0"..<"14.0.0"),
-        .package(url: "https://github.com/Mintegral-official/MintegralAdSDK-Swift-Package", "7.6.7"..<"9.0.0"),
-        .package(url: "https://github.com/mobilefuse/mobilefuse-ios-sdk-spm", from: "1.10.0"),
-        .package(url: "https://github.com/facebook/FBAudienceNetwork", from: "6.21.0"),
         .package(url: "https://github.com/moloco/moloco-sdk-ios-spm", from: "4.4.1")
     ],
     targets: [
@@ -101,41 +61,11 @@ let package = Package(
                 "NimbusKit"
             ]),
         .target(
-            name: "NimbusRenderFANKit",
-            dependencies: ["NimbusRenderTarget", .Facebook]),
-        .target(
-            name: "NimbusRequestFANKit",
-            dependencies: ["NimbusRequestTarget", .Facebook]),
-        .target(
-            name: "NimbusGAMKit",
-            dependencies: ["NimbusTarget", .GoogleMobileAds]),
-        .target(
             name: "NimbusAdMobKit",
             dependencies: ["NimbusTarget", .GoogleMobileAds]),
         .target(
-            name: "NimbusGoogleKit",
-            dependencies: ["NimbusTarget", .GoogleMobileAds]),
-        .target(
-            name: "NimbusLiveRampKit",
-            dependencies: ["NimbusRequestTarget", .LiveRamp]),
-        .target(
-            name: "NimbusRequestAPSKit",
-            dependencies: ["NimbusRequestTarget"]),
-        .target(
-            name: "NimbusUnityKit",
-            dependencies: ["NimbusRenderTarget", "NimbusRequestTarget"]),
-        .target(
-            name: "NimbusMintegralKit",
-            dependencies: ["NimbusRenderTarget", "NimbusRequestTarget", .Mintegral]),
-        .target(
-            name: "NimbusMobileFuseKit",
-            dependencies: ["NimbusRenderTarget", "NimbusRequestTarget", .MobileFuse]),
-        .target(
             name: "NimbusMolocoKit",
             dependencies: ["NimbusRenderTarget", "NimbusRequestTarget", .Moloco]),
-        .target(
-            name: "NimbusInMobiKit",
-            dependencies: ["NimbusRenderTarget", "NimbusRequestTarget", "InMobiSDK"]),
         .target(
             name: "NimbusVungleKit",
             dependencies: ["NimbusRenderTarget", "NimbusRequestTarget", .Vungle]),
@@ -143,18 +73,6 @@ let package = Package(
             name: "OMSDK_Adsbynimbus",
             url: "https://adsbynimbus-public.s3.us-east-1.amazonaws.com/iOS/external/omsdk/1.6.5/OMSDK_Adsbynimbus-1.6.5.zip",
             checksum: "0948cf0e1d68d53904d7f8c96703457496adc1b0d8f10782d59409b0f7a6e974"),
-        .binaryTarget(
-             name: "DTBiOSSDK",
-             url: "https://d14jk8f50gmy3e.cloudfront.net/iOS_APS_SDK/APS_iOS_SDK-5.3.3.zip",
-             checksum: "8e63092764121356b6e3e56d6f1f4b108ba602fb20b93373a5129d3902f77742"),
-        .binaryTarget(
-            name: "UnityAds",
-            url: "https://github.com/Unity-Technologies/unity-ads-ios/releases/download/4.15.1/UnityAds.zip",
-            checksum: "14bf337196be779f91f894c6bc919b3394cf599a6c3a2e3da434773906b68a68"),
-        .binaryTarget(
-            name: "InMobiSDK",
-            url: "https://dl.inmobi.com/inmobi-sdk/IM/InMobi-iOS-SDK-10.8.6.zip",
-            checksum: "ab0f05cd8aa0a7b1085a2b4f57f06ba27ae5dc310e1d9c1241011bba6ba98949")
     ]
 )
 
@@ -169,19 +87,12 @@ extension Target {
 }
 
 extension Target.Dependency {
-    static let LiveRamp = product(name: "LRAtsSDK", package: "ats-sdk-ios")
     static let MockingBird = product(name: "Mockingbird", package: "Mockingbird")
     static let Vungle = product(name: "VungleAdsSDK", package: "VungleAdsSDK-SwiftPackageManager")
     static let GoogleMobileAds = product(
         name: "GoogleMobileAds",
         package: "swift-package-manager-google-mobile-ads"
     )
-    static let Mintegral = product(
-        name: "MintegralAdSDK",
-        package: "MintegralAdSDK-Swift-Package"
-    )
-    static let MobileFuse = product(name: "MobileFuseSDK", package: "mobilefuse-ios-sdk-spm")
-    static let Facebook = product(name: "FBAudienceNetwork", package: "FBAudienceNetwork")
     static let Moloco = product(name: "MolocoSDK", package: "moloco-sdk-ios-spm")
 }
 
